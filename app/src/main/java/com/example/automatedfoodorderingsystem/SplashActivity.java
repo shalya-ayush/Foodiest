aea1863d0c8a9c1dd10aa905ad1f1ebf26863736
shalya21 <ayush.shalya_cs18@gla.ac.in>
2020-08-27 13:30:40 +0530

    Basic layout of Splash Activity

---
+++ app/src/main/java/com/example/automatedfoodorderingsystem/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.automatedfoodorderingsystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
