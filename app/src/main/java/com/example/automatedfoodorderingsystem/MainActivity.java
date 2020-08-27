462dc69adedc6f7f6258d8691f8582161d9b94c3
shalya21 <ayush.shalya_cs18@gla.ac.in>
2020-08-27 13:21:52 +0530

    Layout to separate user and restaurant owner

--- app/src/main/java/com/example/automatedfoodorderingsystem/MainActivity.java
+++ app/src/main/java/com/example/automatedfoodorderingsystem/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.automatedfoodorderingsystem;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.automatedfoodorderingsystem.restaurantPart.RestaurantLoginActivity;
+import com.example.automatedfoodorderingsystem.userPart.UserLoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button user, restaurant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        user = findViewById(R.id.user);
+        restaurant = findViewById(R.id.restaurant);
+        user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, UserLoginActivity.class));
+                finish();
+            }
+        });
+        restaurant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RestaurantLoginActivity.class));
+                finish();
+            }
+        });
+
     }
 }
