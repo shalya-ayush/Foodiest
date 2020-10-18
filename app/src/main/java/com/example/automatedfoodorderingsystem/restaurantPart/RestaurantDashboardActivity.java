d9c2c7e739f54d2bb206fb1eadf0caf2e387d514
shalya21 <ayush.shalya_cs18@gla.ac.in>
2020-10-18 14:28:15 +0530

    Restaurant part java files

---
+++ app/src/main/java/com/example/automatedfoodorderingsystem/restaurantPart/RestaurantDashboardActivity.java
@@ -0,0 +1,16 @@
+package com.example.automatedfoodorderingsystem.restaurantPart;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.automatedfoodorderingsystem.R;
+
+public class RestaurantDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_dashboard);
+    }
+}
