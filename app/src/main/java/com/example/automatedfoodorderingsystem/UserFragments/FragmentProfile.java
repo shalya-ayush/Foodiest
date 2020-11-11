53d4895b0fd4f7f4a7c0f79f880ea3dd586ef304
shalya21 <ayush.shalya_cs18@gla.ac.in>
2020-11-11 10:17:18 +0530

    Profile Fragment completed

--- app/src/main/java/com/example/automatedfoodorderingsystem/UserFragments/FragmentProfile.java
+++ app/src/main/java/com/example/automatedfoodorderingsystem/UserFragments/FragmentProfile.java
@@ -1,5 +1,6 @@
 package com.example.automatedfoodorderingsystem.UserFragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,22 +8,39 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.automatedfoodorderingsystem.Model.UserDatabase;
 import com.example.automatedfoodorderingsystem.R;
+import com.example.automatedfoodorderingsystem.userPart.EditProfileActivity;
+import com.example.automatedfoodorderingsystem.userPart.UserLoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class FragmentProfile extends Fragment {
+    TextView userName;
+    TextView userMobile;
     CircleImageView profileImage;
     TextView editProfile;
-    View viewLine;
     TextView orderHistory;
     TextView shareApp;
     TextView reportBug;
     TextView rateApp;
     Button logoutBtn;
+    FirebaseAuth mAuth;
+    DatabaseReference reference;
+    FirebaseUser mUser;
 
 
     @Override
@@ -30,14 +48,70 @@ public class FragmentProfile extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //////////// Firebase hooks //////////////
+        mAuth = FirebaseAuth.getInstance();
+        reference = FirebaseDatabase.getInstance().getReference().child("UsersDatabase");
+        mUser = mAuth.getCurrentUser();
+
+        ////////////hooks ////////////////////
+        userName = view.findViewById(R.id.userName);
+        userMobile = view.findViewById(R.id.userMobile);
         profileImage = view.findViewById(R.id.user_profile_image);
+
+        //////// Edit Profile Button ///////////
         editProfile = view.findViewById(R.id.user_profile_edit);
-        viewLine = view.findViewById(R.id.single_line);
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), EditProfileActivity.class));
+
+            }
+        });
         orderHistory = view.findViewById(R.id.user_profile_order);
         shareApp = view.findViewById(R.id.user_share);
         reportBug = view.findViewById(R.id.user_report);
         rateApp = view.findViewById(R.id.user_rate_app);
+
+        /////// Logout Button //////////
         logoutBtn = view.findViewById(R.id.user_logout_btn);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+
+        /////// Method to show user name and mobile number ///////
+        showUserName();
         return view;
+    }
+
+    ///////Method to show user name and mobile number /////////
+    private void showUserName() {
+        reference.child(mUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UserDatabase user = snapshot.getValue(UserDatabase.class);
+                userName.setText(user.getName());
+                userMobile.setText(user.getPhoneNo().substring(3));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    //////// Method to Logout User ////////////
+    private void logoutUser() {
+        mAuth.signOut();
+        Intent intent = new Intent(getActivity(), UserLoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        Objects.requireNonNull(getActivity()).finish();
+
     }
 }
