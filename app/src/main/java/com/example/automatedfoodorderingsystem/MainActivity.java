a228772575c1b9563bac44f9e741362b370002a8
shalya21 <ayush.shalya_cs18@gla.ac.in>
2020-11-11 10:22:43 +0530

    Main Activity java file

--- app/src/main/java/com/example/automatedfoodorderingsystem/MainActivity.java
+++ app/src/main/java/com/example/automatedfoodorderingsystem/MainActivity.java
@@ -4,14 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.automatedfoodorderingsystem.restaurantPart.RestaurantLoginActivity;
 import com.example.automatedfoodorderingsystem.userPart.UserDashboardActivity;
 import com.example.automatedfoodorderingsystem.userPart.UserRegistrationActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener authStateListener;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +49,42 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        mAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                mUser = FirebaseAuth.getInstance().getCurrentUser();
+                if (mUser != null) {
+                    Toast.makeText(MainActivity.this, "Verified User", Toast.LENGTH_SHORT).show();
+                    sendUserToDashboardActivity();
+                    finish();
 
+                }
+
+            }
+        };
+
+    }
+
+    private void sendUserToDashboardActivity() {
+        Intent intent = new Intent(MainActivity.this, UserDashboardActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mAuth != null) {
+            mAuth.addAuthStateListener(authStateListener);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(authStateListener);
     }
 }
